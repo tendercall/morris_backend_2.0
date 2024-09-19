ad06fc57f3b3ae5a3a223043fcf9d3508b4611dd
tendercall <info@tendercall.com>
2024-09-19 13:26:22 +0530

    changes

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module morris-backend.com/main
 
 go 1.21.0
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/aws/aws-sdk-go v1.55.5
+	github.com/lib/pq v1.10.9
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- services/handler/handler.go
+++ services/handler/handler.go
@@ -1,11 +1,22 @@
 package handler
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"image"
+	"image/jpeg"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/nfnt/resize"
 	"morris-backend.com/main/services/helper"
 	"morris-backend.com/main/services/models"
 )
@@ -159,16 +170,84 @@ func BannerHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func PostBannerHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20) // 10MB max file size
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
 
 	var Banner models.Banner
 
-	if err := json.NewDecoder(r.Body).Decode(&Banner); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	// Process uploaded image
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Error uploading file", http.StatusBadRequest)
+		fmt.Println("Error uploading file:", err)
+		return
+	}
+	defer file.Close()
+
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Error reading file content", http.StatusInternalServerError)
+		fmt.Println("Error reading file content:", err)
+		return
 	}
 
-	err := helper.PostBanner(Banner.Image, Banner.CreatedDate)
+	// Resize image if it exceeds 3MB
+	if len(fileBytes) > 3*1024*1024 {
+		img, _, err := image.Decode(bytes.NewReader(fileBytes))
+		if err != nil {
+			http.Error(w, "Error decoding image", http.StatusInternalServerError)
+			fmt.Println("Error decoding image:", err)
+			return
+		}
+
+		newImage := resize.Resize(800, 0, img, resize.Lanczos3)
+		var buf bytes.Buffer
+		err = jpeg.Encode(&buf, newImage, nil)
+		if err != nil {
+			http.Error(w, "Error encoding compressed image", http.StatusInternalServerError)
+			fmt.Println("Error encoding compressed image:", err)
+			return
+		}
+		fileBytes = buf.Bytes()
+	}
+
+	// Upload image to AWS S3
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"), // Replace with your AWS region
+		Credentials: credentials.NewStaticCredentials(
+			"AKIAYS2NVN4MBSHP33FF",                     // Replace with your AWS access key ID
+			"aILySGhiQAB7SaFnqozcRZe1MhZ0zNODLof2Alr4", // Replace with your AWS secret access key
+			""), // Optional token, leave blank if not using
+	})
+	if err != nil {
+		log.Printf("Failed to create AWS session: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	svc := s3.New(sess)
+	imageKey := fmt.Sprintf("BannerImage/%d.jpg", time.Now().Unix()) // Adjust key as needed
+	_, err = svc.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String("tendercall-db"), // Replace with your S3 bucket name
+		Key:    aws.String(imageKey),
+		Body:   bytes.NewReader(fileBytes),
+	})
+	if err != nil {
+		log.Printf("Failed to upload image to S3: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	// Construct imageURL assuming it's from your S3 bucket
+	imageURL := fmt.Sprintf("https://tendercall-db.s3.amazonaws.com/%s", imageKey)
+
+	err = helper.PostBanner(imageURL, Banner.CreatedDate)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
